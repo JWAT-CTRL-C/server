b1e85d39f19446e4131be7fccac30baf2a1da11c
ZLoganZ <ln26805@gmail.com>
2024-05-28 17:09:54 +0700

    [chore]: Update npm dependencies and add @nestjs/config and @nestjs/mapped-types

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
+import helmet from 'helmet';
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const config = new ConfigService();
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.enableCors();
+  app.use(helmet());
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+  await app.listen(config.get<number>('port'));
 }
 bootstrap();
